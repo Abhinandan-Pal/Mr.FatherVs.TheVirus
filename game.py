147d18d855a76f0a95a144e25ed92b736170e839
Abhinandan-Pal <abhinandan.mike123@gmail.com>
2020-04-08 17:38:50 +0530

    added game.py

--- game.py
+++ game.py
@@ -15,8 +15,15 @@ population_upper = 10000
 population_lower =5000
 money_upper = 700
 money_lower = 30
-fest_social_realation = 0.2
 no_of_countries = 10
+
+fest_social_relation = 0.2
+money_product_relation = 2
+population_product_relation = 75000
+infect_social_relation = 0.1
+infect_hygine_relation = 0.2
+lock_support_relation = 0.3
+travel_imp_exp_val = 500000
 #---------------------xxxxxxxxxxxxxxx-----------------------------------
 
 
@@ -40,7 +47,10 @@ class Country(NamedTuple):
     infected_arr : list
     dead_arr : list
     recovered_arr : list
-
+    productivity : float
+    infectivity : float
+    lock_down_ratio : float
+    
 player_index = -1
 data_accuracy_value = 0.25
 #---------------------xxxxxxxxxxxxxxx-----------------------------------
@@ -51,8 +61,8 @@ def generate_country(no_of_countries):
     hygine_value = random.randint(10, 30)/100
     money = random.randint(money_lower, money_upper)
     support = random.randint(60, 90)/100
-    festivity = random.randint(20, 80)/100
-    socialization = random.randint(30, 50)/100 + fest_social_realation*festivity
+    festivity = random.randint(10, 50)/100
+    socialization = random.randint(30, 50)/100 + fest_social_relation*festivity
     info_accuracy = random.randint(30, 90)/100
     export_v = random.sample(range(100), no_of_countries)
     import_v = random.sample(range(100), no_of_countries)
@@ -64,7 +74,10 @@ def generate_country(no_of_countries):
     infected_arr =[infected]
     dead_arr =[dead]
     recovered_arr =[recovered]
-    country = Country(population,hygine_value,money,support,festivity,socialization,info_accuracy,export_v,import_v,infected,dead,recovered,population_arr,money_arr,infected_arr,dead_arr,recovered_arr)
+    productivity = random.randint(10, 50)/100
+    infectivity = random.randint(0, 10)/100 + infect_social_relation*socialization - infect_hygine_relation*hygine_value
+    lock_down_ratio =random.randint(50, 70)/100+ lock_support_relation*support
+    country = Country(population,hygine_value,money,support,festivity,socialization,info_accuracy,export_v,import_v,infected,dead,recovered,population_arr,money_arr,infected_arr,dead_arr,recovered_arr,productivity,infectivity,lock_down_ratio)
     return country
 def big_bang():
 
@@ -91,8 +104,24 @@ def plot(x,label_y,is_mine,title):
     plt.title(title)
     plt.show()
 
-def change_param():
-    print('5')
+def travel_effect(c1,index):
+    for i in range(no_of_countries):
+        if (i == index):
+            continue;
+        export_i = c1.export_v[i]
+        c2 = Countries_data[i]
+        import_i = c2.import_v[index]
+        c1.population = c1.population - c1.population*export_i*import_i/travel_imp_exp_val
+        c2.population = c2.population + c1.population*export_i*import_i/travel_imp_exp_val
+        c1.infected = c1.infected - c1.infected*export_i*import_i/travel_imp_exp_val
+        c2.infected = c2.infected + c1.infected*export_i*import_i/travel_imp_exp_val
+
+def change_param(c,index):
+    c.socialization = c.socialization + fest_social_relation*c.festivity
+    c.infectivity = c.infectivity + infect_social_relation*c.socialization - infect_hygine_relation*c.hygine_value
+    c.money =  c.money + c.productivity*money_product_relation*c.population/population_product_relation 
+    travel_effect(c,index)
+
 
 def country_update():
     print('5')
@@ -115,7 +144,6 @@ def data_country_view(country,is_mine,name):
     plot(country.dead_arr,"Dead",is_mine,name)
     plot(country.recovered_arr,"Recovered",is_mine,name)
     
-country = Countries_data[4]
 def view_data():
     a = 0
     while(a != -1):
